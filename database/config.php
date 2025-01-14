42014c8b528b00883987a7ecd2904c60696af60d
Your Name <you@example.com>
2025-01-14 12:01:38 +0000

    commit 4

---
+++ database/config.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$hn = "localhost";
+$un = "cian_admin";
+$pw = "e1T6()tE[/MvD5cB";
+$db = "musiccm";
+
+// Create database connection
+$conn = new mysqli($hn, $un, $pw, $db);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $db->connect_error);
+}
+else
+{
+    echo 'Connection Successful'
+}
+
+?>
