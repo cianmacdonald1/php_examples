bd0efe3cadf1dbafe602441c995e897e7b5fcbe6
Your Name <you@example.com>
2025-01-14 11:35:09 +0000

    commit 3

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+include 'partials/header.php';
+?>
+
+<?php
+function calculateArea($length, $width) 
+{
+$area = $length $width;
+return $area;
+}
+$result = calculateArea(5.10);
+echo "The area is: " . $result; //Output: The Area is: 50
+?>
+
+<?php
+include 'partials/footer.php';
+?>
